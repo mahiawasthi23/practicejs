5c51c2480516b6a88d40622e5ba40d232cdd45fb
mahima <mahima@23navgurukul.org>
2024-04-02 11:13:02 +0530

    practicepage

---
+++ practice.js
@@ -0,0 +1,76 @@
+// for(let i=0; i<5; i++){
+//     console.log(i);
+// }
+
+// let i=0;
+// for(; i<5; i++){
+//     console.log(i);
+// }
+
+// for(let i=0; i<5;){
+//     console.log(i);
+//     i++;
+// }
+
+// let i=0;
+// for(; i<5; i++){
+//     console.log(i);
+// }
+// console.log(i);
+
+// let i=0;
+// while(i<5){
+//     console.log(i);
+//     i++;
+// }
+
+
+// for (let i=0; i<10; i++){
+//     if(i===3){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// let i=0;
+// while(i<5){
+//     if(i===2){
+//         i++;
+//         continue;
+//     }
+//     console.log(i);
+//     i++;
+// }
+
+// for (let i=0; i<10; i++){
+//     if(i===5){
+//         break;
+//     }
+//     console.log(i);
+// }
+
+// let i=0;
+// while(i<10){
+//     console.log(i);
+//     i++;
+//     if(i ===5){
+//         break;
+//     }
+// }
+
+// let i=0;
+// while(i<10){
+//     if(i === 5){
+//         break;
+//     }
+//     console.log(i);
+//     i++;
+// }
+
+
+// let i=0;
+// do {
+//     console.log(i);
+//     i++;
+// }
+// while(i<0);
