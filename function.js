1692dda97ca272394461719b4ce99375d08980c4
mahima <mahima@23navgurukul.org>
2024-05-05 13:49:26 +0530

    function

---
+++ function.js
@@ -0,0 +1,41 @@
+// function addTonumber(a,b){
+//     return a+b
+// }
+// console.log(addTonumber(25,7));
+
+
+// function Hello(){
+//     return "hello"
+// }
+// console.log(Hello());
+
+
+
+// function Name(tajmahel){
+//     tajmahel();
+//     console.log("Mahima");
+// }
+
+// function message(){
+//     console.log("Good Night");
+// }
+// Name(message);
+
+
+
+// function sum(){
+//     function printfunction(a,b){
+//         return a + b;
+//     }
+//    return printfunction;
+// }
+
+// const add  = sum();   
+// console.log(add(5 ,6));
+
+
+// let Number = function(){
+//     return 5;
+// }
+
+// console.log(Number());
--- practice.js
+++ practice.js
@@ -74,3 +74,19 @@
 //     i++;
 // }
 // while(i<0);
+
+// function place(city ,country){
+//     country = country || "China";
+//     console.log("I belong to" + " " + city +" "+ "in" +" " + country);
+// }
+// place("Delhi");
+
+// function passingArrays(arr){
+//     let i=0;
+//     let p = arr-length;
+//     while(i<p){
+//         console.log(arr[i]);
+//         i++;
+//     }
+// }
+// passingArrays([10,20,30,40,50,60]);
