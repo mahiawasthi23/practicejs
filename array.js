a5563552ef3d9342c0ee4e589cf6347b48dfcb49
mahima <mahima@23navgurukul.org>
2024-06-01 09:52:53 +0530

    array questions

---
+++ array.js
@@ -0,0 +1,21 @@
+// const array = [1, 5, 6, 3, 5, 6];
+// let c = 0;
+// for (let i=0; i<array.length; i++){
+//     if(array[i] >= 0){
+//         c++;
+//     }
+//     else{
+//         break;
+//     }
+// }
+// console.log(c);
+
+
+// const array = [3, 6, 2, 1, 7, 2, 5, 6, 1];
+// let sum = 0;
+// for (let i=0; i<array.length-1; i=i+2){
+//     if(array[i+1]-array[i]===1){
+//         sum = sum+(array[i+1]+array[i]);
+//     }
+// }
+// console.log(sum);
